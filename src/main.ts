7d89ea3eab6ee45fdfbf6ed9d28115b68461c77e
Kun Yang <yangkun@effyic.com>
2024-04-25 17:16:03 +0800

    Demo

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import ElementPlus from 'element-plus'
 import locale from 'element-plus/dist/locale/zh-cn.mjs'
 import { createPinia } from 'pinia'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import DataVVue3 from '@kjgl77/datav-vue3'
 import router from './router'
 import globalComponent from '@/components/SvgIcon/index'
 import './assets/fonts/fonts.css'
@@ -18,6 +19,7 @@ const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue))
   app.component(key, <Component>component)
 
+app.use(DataVVue3)
 app.use(createPinia())
 app.use(router)
 app.use(ElementPlus, { locale })
